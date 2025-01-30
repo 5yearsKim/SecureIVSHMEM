9dc66645a04a275e60dec432af2c6fbb1ec5e116
onion.kim <onion.kim@intel.com>
2025-01-30 18:28:23 +0900

    Adding a script to move working folder to shared dir
    Signed-off-by: onion.kim <onion.kim@intel.com>

---
+++ ipc_shm_em/scripts/copy_dir_to_shared.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copy a directory to the shared memory
+
+WORK_DIR=$(realpath $(dirname $0)/..)
+TARET_DIR="../qemu/shared/ipc_shm_em"
+
+cd $WORK_DIR
+
+mkdir -p "$TARET_DIR"
+
+cp -r ./* "$TARET_DIR"
+
+echo "Copied all files to $TARET_DIR"
