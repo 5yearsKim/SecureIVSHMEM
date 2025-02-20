07d297d1afc6227acb79bb5577a9f77a3b4a3946
Lee, Jerry J <jerry.j.lee@intel.com>
2025-02-20 18:01:23 +0900

    [Kernel Module] Implemented structure for the hook data
    - using ftrace_ops for finding and hooking the mmap point
    - Wrote the custom structure for handling the sequence
    - Added the Intel license
    
    Signed-off-by: Lee, Jerry J <jerry.j.lee@intel.com>

--- kernel_module/ivshmem_uio_hook.c
+++ kernel_module/ivshmem_uio_hook.c
@@ -1,5 +1,23 @@
 /*
- * copyright: Lee, Jerry J (jerry.j.lee@intel.com)
+ * INTEL CONFIDENTIAL
+ * Copyright (c) 2025 Intel Corporation All Rights Reserved
+ *
+ * The source code contained or described herein and all documents related
+ * to the source code ("Material") are owned by Intel Corporation or its
+ * suppliers or licensors. Title to the Material remains with Intel
+ * Corporation or its suppliers and licensors. The Material contains trade
+ * secrets and proprietary and confidential information of Intel or its
+ * suppliers and licensors. The Material is protected by worldwide copyright
+ * and trade secret laws and treaty provisions. No part of the Material may
+ * be used, copied, reproduced, modified, published, uploaded, posted,
+ * transmitted, distributed, or disclosed in any way without Intel's prior
+ * express written permission.
+ *
+ * No license under any patent, copyright, trade secret or other intellectual
+ * property right is granted to or conferred upon you by disclosure or
+ * delivery of the Materials, either expressly, by implication, inducement,
+ * estoppel or otherwise. Any license under such intellectual property rights
+ * must be express and approved by Intel in writing.
  */
 
 #include <linux/fs.h>
@@ -68,15 +86,106 @@ struct ivshmem_context {
         struct ivshmem_uio_hook_data *hook_data;
 };
 
+static struct ivshmem_context *__ivshmem_ctx_create(void);
+static int __ivshmem_ctx_destroy(struct ivshmem_context *del);
+
+static struct ftrace_ops *__ivshmem_ops_create(void);
+static int __ivshmem_ops_destroy(struct ftrace_ops *del);
+
+static struct ivshmem_uio_hook_data *__ivshmem_hook_data_create(void);
+static int __ivshmem_hook_data_destroy(struct ivshmem_uio_hook_data *del);
+
+static inline int __ivshmem_uio_hook_exit(struct ivshmem_context *ctx);
+
 static struct ivshmem_context *g_ctx = NULL;
 
 static int __init ivshmem_uio_hook_init(void) {
-        FORMULA_GUARD(g_ctx != NULL, -EPERM, ERR_INVALID_PTR);
+        FORMULA_GUARD(g_ctx != NULL, -EINVAL, ERR_INVALID_PTR);
+
+        struct ivshmem_context *ctx = __ivshmem_ctx_create();
+        FORMULA_GUARD_WITH_EXIT_FUNC(ctx == NULL, -ENOMEM,
+                                     __ivshmem_uio_hook_exit, "");
+
+        struct ftrace_ops *ops = __ivshmem_ops_create();
+        FORMULA_GUARD_WITH_EXIT_FUNC(ops == NULL, -ENOMEM,
+                                     __ivshmem_uio_hook_exit, "");
+
+        struct ivshmem_uio_hook_data *hook_data = __ivshmem_hook_data_create();
+        FORMULA_GUARD_WITH_EXIT_FUNC(hook_data == NULL, -ENOMEM,
+                                     __ivshmem_uio_hook_exit, "");
+
+        ctx->ops = ops;
+        ctx->hook_data = hook_data;
+        g_ctx = ctx;
+
         return 0;
 }
 
 static void __exit ivshmem_uio_hook_exit(void) {
-        FORMULA_GUARD(g_ctx == NULL, , ERR_INVALID_PTR);
+        __ivshmem_uio_hook_exit(g_ctx);
+}
+
+static struct ivshmem_context *__ivshmem_ctx_create(void) {
+        struct ivshmem_context *new =
+            kmalloc(sizeof(struct ivshmem_context), GFP_KERNEL);
+        FORMULA_GUARD(new == NULL, NULL, ERR_MEMORY_SHORTAGE);
+
+        return new;
+}
+
+static int __ivshmem_ctx_destroy(struct ivshmem_context *del) {
+        FORMULA_GUARD(del == NULL, -EINVAL, ERR_INVALID_PTR);
+
+        PTR_FREE(del);
+
+        return 0;
+}
+
+static struct ftrace_ops *__ivshmem_ops_create(void) {
+        struct ftrace_ops *new = kmalloc(sizeof(struct ftrace_ops), GFP_KERNEL);
+        FORMULA_GUARD(new == NULL, NULL, ERR_MEMORY_SHORTAGE);
+
+        new->func = NULL;
+        new->flags = FTRACE_OPS_FL_SAVE_REGS | FTRACE_OPS_FL_RECURSION;
+
+        return new;
+}
+
+static int __ivshmem_ops_destroy(struct ftrace_ops *del) {
+        FORMULA_GUARD(del == NULL, -EINVAL, ERR_INVALID_PTR);
+
+        PTR_FREE(del);
+
+        return 0;
+}
+
+static struct ivshmem_uio_hook_data *__ivshmem_hook_data_create(void) {
+        struct ivshmem_uio_hook_data *new =
+            kmalloc(sizeof(struct ivshmem_uio_hook_data), GFP_KERNEL);
+        FORMULA_GUARD(new == NULL, NULL, ERR_MEMORY_SHORTAGE);
+
+        new->limit_map_size = 0;
+        new->origin_mmap = NULL;
+
+        return new;
+}
+
+static int __ivshmem_hook_data_destroy(struct ivshmem_uio_hook_data *del) {
+        FORMULA_GUARD(del == NULL, -EINVAL, ERR_INVALID_PTR);
+
+        PTR_FREE(del);
+
+        return 0;
+}
+
+static inline int __ivshmem_uio_hook_exit(struct ivshmem_context *ctx) {
+        FORMULA_GUARD(ctx == NULL, -EINVAL, ERR_INVALID_PTR);
+
+        __ivshmem_hook_data_destroy(ctx->hook_data);
+        __ivshmem_ops_destroy(ctx->ops);
+        __ivshmem_ctx_destroy(ctx);
+
+        return 0;
 }
 
 module_init(ivshmem_uio_hook_init);
